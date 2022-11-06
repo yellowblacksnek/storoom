efa0bd42df8727e067b3772eb687e8a2dbf4394d
sus <truemarat@gmail.com>
2022-11-06 21:27:38 +0300

    test(basic): fix contextLoads test unable to pass

--- src/test/java/ru/itmo/highload/storoom/StoroomApplicationTests.java
+++ src/test/java/ru/itmo/highload/storoom/StoroomApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StoroomApplicationTests {
+class StoroomApplicationTests extends BaseTests{
 
     @Test
     void contextLoads() {
