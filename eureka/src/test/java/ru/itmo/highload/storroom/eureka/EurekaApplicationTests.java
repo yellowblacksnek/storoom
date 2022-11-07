04a09f2a0e5fb734196644bef051112e58dc7b54
sus <truemarat@gmail.com>
2022-11-07 20:24:25 +0300

    feat(initial-setup): add eureka server and gateway service

---
+++ eureka/src/test/java/ru/itmo/highload/storroom/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.highload.storroom.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gateway/src/main/java/ru/itmo/highload/storoom/gateway/SpringCloudConfig.java
@@ -0,0 +1,19 @@
+package ru.itmo.highload.storoom.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/token/**")
+                        .uri("lb://TOKEN-SERVICE"))
+                .build();
+    }
+
+}
